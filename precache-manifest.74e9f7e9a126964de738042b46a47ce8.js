44deb1a59c1821eb214f5344f37e52847c5c1074
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-22 04:02:11 +0000

    Deploy malimuradov/week05_comp3104_lab_exec to github.com/malimuradov/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.74e9f7e9a126964de738042b46a47ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2ee458ed1b7f162557",
+    "url": "/static/css/main.6b1086ad.chunk.css"
+  },
+  {
+    "revision": "7e2ee458ed1b7f162557",
+    "url": "/static/js/main.7e2ee458.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "0e072631b1c7361069bb171a642674e9",
+    "url": "/index.html"
+  }
+];
